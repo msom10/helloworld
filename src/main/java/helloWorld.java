ebb9b7aad594685aecb2f2bd45f096e5260d419e
Mohanish Soma <mohanish.soma@gmail.com>
2016-11-28 10:40:54 +1100

    First Commit1
    Changed name and email

---
+++ src/main/java/helloWorld.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Created by ms392556 on 22/11/2016.
+ */
+
+
+
+public class helloWorld {
+
+    public static void main(String [ ] args)
+    {
+        System.setProperty("webdriver.chrome.driver", "H:\\Auto\\SetUps\\chrome2.20\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://crownbet.com.au/");
+
+
+
+
+    }
+
+
+}
